8d8ec232ec04c2047eebec1af8592a2476847912
dmarhp <dmarhp@gmail.com>
2026-03-25 19:00:01 +0200

    initial

--- stencil.config.ts
+++ stencil.config.ts
@@ -13,7 +13,7 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/',
+      baseUrl: 'https://dmarhp.github.io/brick-game/',
     },
   ],
   plugins: [
